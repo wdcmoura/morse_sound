972b8797bafdaf617f98cc79bdf6fb993e7be538
William Moura <wdcmoura@gmail.com>
2025-12-17 12:28:48 -0300

    Atualizçaões

--- api/beep.js
+++ api/beep.js
@@ -1,14 +1,22 @@
-let lastBeep = 0;
+let lastBeep = {
+  count: 0,
+  seq: 0
+};
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
-    const { count } = req.body;
-    lastBeep = count;
+    const { count, seq } = req.body;
+
+    lastBeep = {
+      count,
+      seq
+    };
+
     return res.status(200).json({ ok: true });
   }
 
   if (req.method === 'GET') {
-    return res.status(200).json({ count: lastBeep });
+    return res.status(200).json(lastBeep);
   }
 
   res.status(405).end();
